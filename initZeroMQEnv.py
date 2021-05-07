60787f0bda9f09be0d9d295184456e325e0253c0
赵睿 <zrendless@foxmail.com>
2021-05-07 14:05:25 +0800

    添加测试项目

---
+++ initZeroMQEnv.py
@@ -0,0 +1,21 @@
+import winreg
+import os
+
+LIB_DIR_Path = r"D:\codeSofts\ZeroMQ\CMake"
+
+def createOpenVINODirPath():
+    # 系统环境变量
+    # parentKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"\SYSTEM\CurrentControlSet\Control\Session Manager\Environment",
+                        #  0, winreg.KEY_ALL_ACCESS | winreg.KEY_WOW64_64KEY)
+    key=winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE,r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment")
+    winreg.SetValueEx(key,"ZeroMQ_DIR",0,winreg.REG_SZ,LIB_DIR_Path)
+    winreg.CloseKey(key)
+    print("create INTEL_OPENVINO_DIR done")
+    return
+
+if __name__=="__main__":
+    if(not os.path.exists(LIB_DIR_Path)):
+        # 如果不存在
+        print(LIB_DIR_Path," not exists")
+    else:
+        createOpenVINODirPath()
