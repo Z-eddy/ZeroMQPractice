041ed1a507bc26d74cc8f22676182de02dd65a4f
赵睿 <zrendless@foxmail.com>
2021-05-10 14:22:11 +0800

    测试proto的序列化、反序列化

--- src/ClientTest/main.cpp
+++ src/ClientTest/main.cpp
@@ -1,27 +1,52 @@
 ﻿#include <iostream>
 #include <string>
 
+#include "EmployeeTest.pb.h"
 #include "zmq.hpp"
 #include "zmq_addon.hpp"
+using std::cout;
+using std::endl;
+using std::ends;
+using namespace EmployeeTest;
 
 int main() {
+  //发送的数据
+  EmployeeTest::Employee myData;
+  myData.set_id(0);
+  myData.set_age(16);
+  myData.set_name("my employee name");
+  //序列化
+  auto tempData{myData.SerializeAsString()};
+  cout << "the serialize data size:" << tempData.size() << endl;
+
+  //解析的数据
+  EmployeeTest::Employee getMyData;
+  //反序列化
+  getMyData.ParseFromString(tempData);
+  cout << getMyData.id() << ends << getMyData.age() << ends << getMyData.name()
+       << endl;
+
   // initialize the zmq context with a single IO thread
   zmq::context_t context{1};
 
   // construct a REQ (request) socket and connect to interface
-  zmq::socket_t socket{context, zmq::socket_type::sub};
-  socket.connect("tcp://192.168.0.13:9092");
-  socket.set(zmq::sockopt::subscribe, "");
+  zmq::socket_t socket{context, zmq::socket_type::req};
+  socket.connect("tcp://localhost:5555");
+  // socket.set(zmq::sockopt::subscribe, "");
 
-  while (true) {
-    // Receive all parts of the message
-    std::vector<zmq::message_t> recv_msgs;
-    zmq::recv_result_t result =
-        zmq::recv_multipart(socket, std::back_inserter(recv_msgs));
+  const std::string data{"Hello"};
+  for (auto request_num = 0; request_num < 10; ++request_num) {
+    // send the request message
+    std::cout << "Sending Hello " << request_num << "..." << std::endl;
+    socket.send(zmq::buffer(data), zmq::send_flags::none);
 
-    for (const auto &item : recv_msgs) {
-      std::cout << item.to_string() << std::endl;
-    }
+    // wait for reply from server
+    zmq::message_t reply{};
+    socket.recv(reply, zmq::recv_flags::none);
+
+    std::cout << "Received " << reply.to_string();
+    std::cout << " (" << request_num << ")";
+    std::cout << std::endl;
   }
 
   return 0;
