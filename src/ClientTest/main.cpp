7e2b66a72032285ce84d4e91672208f13a9755fc
赵睿 <zrendless@foxmail.com>
2021-05-18 11:36:51 +0800

    测试顶层Data转换指定类型

--- src/ClientTest/main.cpp
+++ src/ClientTest/main.cpp
@@ -4,6 +4,7 @@
 #include "EmployeeTest.pb.h"
 #include "zmq.hpp"
 #include "zmq_addon.hpp"
+#include "TopData.pb.h"
 using std::cout;
 using std::endl;
 using std::ends;
@@ -17,12 +18,22 @@ int main() {
   myData.set_name("test wang yi yun");
   //序列化
   auto tempData{myData.SerializeAsString()};
-  cout << "the serialize data size:" << tempData.size() << endl;
+
+  TrZeroMQMsg::TopData topData;
+  topData.set_type("Employee");
+  topData.set_rawdata(tempData);
+  //序列化
+  auto serialData{topData.SerializeAsString()};
+
+  TrZeroMQMsg::TopData tempTopData;
+  //反序列化
+  tempTopData.ParseFromString(serialData);
+  std::cout<<tempTopData.type()<<std::endl;
 
   //解析的数据
   EmployeeTest::Employee getMyData;
   //反序列化
-  getMyData.ParseFromString(tempData);
+  getMyData.ParseFromString(tempTopData.rawdata());
   cout << getMyData.id() << ends << getMyData.age() << ends << getMyData.name()
        << endl;
 
@@ -31,15 +42,15 @@ int main() {
 
   // construct a REQ (request) socket and connect to interface
   zmq::socket_t socket{context, zmq::socket_type::req};
-  socket.connect("tcp://localhost:5555");
+  socket.connect("tcp://localhost:9527");
   // socket.set(zmq::sockopt::subscribe, "");
 
   const std::string data{"Hello"};
   for (auto request_num = 0; request_num < 10; ++request_num) {
     // send the request message
-    std::cout << "Sending Hello " << request_num << "..." << std::endl;
+    //std::cout << "Sending Hello " << request_num << "..." << std::endl;
     //socket.send(zmq::buffer(data), zmq::send_flags::none);
-    socket.send(zmq::buffer(tempData), zmq::send_flags::none);
+    socket.send(zmq::buffer(serialData), zmq::send_flags::none);
 
     // wait for reply from server
     zmq::message_t reply{};
